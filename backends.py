cbe1e64f8e15395cdbf9931dde8b3020ba4bcba3
AmdaUwU <jbat2000@gmail.com>
2023-03-31 21:43:40 -0400

    feat(backends.py): add support for loading presets from a JSON file and using them to set up the discussion before it starts. Add a new argument to the gpt35 class constructor to specify the preset to use. If a discussion is passed as an argument, it will override the preset. Update the send_request method to only take a message argument and remove the discussion argument.

--- backends.py
+++ backends.py
@@ -15,18 +15,36 @@ class Meeseeks:
     def create_new_Meeseeks(self):
         pass
 
+    def load_preset(self, preset):
+        '''Sets up the "preset", aka info before the discussion starts'''
+        presets = {}
+        with open(f'{script_dir}/gpt_presets.json') as read:
+            presets = json.load(read)
+        preset_list = list(presets)
+
+        self.discussion = presets[preset]['prompt']
+
+        if 'data' in presets[preset]:
+            for data_name, data_command in presets[preset]['data'].items():
+                result = subprocess.run([data_command],
+                                        shell=True,
+                                        capture_output=True).stdout.decode()
+                self.discussion[0]["content"] = discussion[0]["content"].replace(
+                    '{' + str(data_name) + '}', result)
+
 # gpt 3.5 backend -------------------------------------------------------------
 
 class gpt35(Meeseeks):
 
     endpoint = "https://api.openai.com/v1/chat/completions"
 
-    def __init__(self, api_key: str = '', max_number_of_tries: int = 3, temp: int = 1, length=100,timeout=10):
+    def __init__(self, api_key: str = '', max_number_of_tries: int = 3, temp: int = 1, length=100, timeout=10, preset='default', discussion=None):
         if api_key == '':
             with open(f"{script_dir}/open_ai.secrets") as secret:
                 self.api_key = secret.readline().strip('\n')
         else:
             self.api_key = api_key
+
         self.max_number_of_tries = max_number_of_tries
         self.headers = {
             "Content-Type": "application/json",
@@ -36,7 +54,13 @@ class gpt35(Meeseeks):
         self.length = length
         self.timeout = timeout
 
-    def send_request(self, message:str, discussion:list):
+        # preset argument will be overident by discussion
+        if discussion:
+            self.discussion = discussion if dicussion
+        else:
+            self.load_preset(preset)
+
+    def send_request(self, message:str) -> str:
 
         message_user = {"role": "user", "content": message}
 
@@ -72,22 +96,6 @@ class gpt35(Meeseeks):
 
         with open('temp.txt', 'w') as file:
             file.write(content_assistant)
-
-    # def gpt35_req(data, headers, backend):
-        # '''Attempts to send the request to the backend'''
-        # for _ in range(max_number_of_tries):
-            # response = requests.post(endpoint,
-                                    # headers=headers,
-                                    # data=json.dumps(data),
-                                    # timeout=args.t)
-            # match response.status_code:
-                # case 200:
-                    # message = response.json()['choices'][-1]['message']['content']
-                    # return message
-                # case _:
-                    # print(f"an oupise of type {response.status_code} happend, retrying...")
-
-        # sys.exit(1)
 
 class llama(Meeseeks):
     pass
