582dfec3cdb590e45b42ab79bcc7260492b07ca2
AmdaUwU <jbat2000@gmail.com>
2023-04-14 11:59:45 -0400

    refactor(custom_logging.py): add more intuitive function to call the logger feat(gpt_ping): add custom logging to gpt_ping script and use it instead of the old logger. Also add a warning message when using the terminal feature.

--- custom_logging.py
+++ custom_logging.py
@@ -8,3 +8,21 @@ _ = logger.add(
     format="<b><yellow>{level}</yellow></b>: {message}",
     filter=lambda record: record["level"].name == "system",
 )
+logger.level("danger", no=38, color="<red>")
+_ = logger.add(
+    sys.stderr,
+    format="<b><red>DANGER</red></b>: {message}",
+    filter=lambda record: record["level"].name == "danger",
+)
+
+# more "intuitive" fucntion to call the logger
+
+log_types = ["system", "danger"]
+
+for log_type in log_types:
+    exec(
+        f"""
+def {log_type}(message: str):
+    logger.log("{log_type}", message)
+"""
+    )
